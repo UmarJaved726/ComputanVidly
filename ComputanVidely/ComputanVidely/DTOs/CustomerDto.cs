87803bdffea5da362631b865f400136f43df5be8
Umar Javed <umarjavd728@gmail.com>
2020-08-17 15:00:03 +0500

    Add Customer DTO

---
+++ ComputanVidely/ComputanVidely/DTOs/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComputanVidely.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ComputanVidely.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18IfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
